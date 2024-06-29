208e09a5ffcd70a38bc88bc0d917f3ebcd2ff915
pawkw <pawkw@users.noreply.github.com>
2024-06-29 19:26:28 -0400

    Initial copy pasta + TokenBuffer init_patterns, load_files, config

---
+++ TokenBuffer.py
@@ -0,0 +1,70 @@
+from typing import List, Dict
+from tokenizer import tokenize, Token
+import re
+
+class TokenBuffer:
+    def __init__(self):
+        self.column = 0
+        self.line = 0
+        self.files = []
+        self.file_index = 0
+        self.lines: List[List[Token]] = [[]]
+        self.config = {
+            'skip_white_space': False,
+            'skip_EOF': True
+        }
+
+    def init_patterns(self, patterns: Dict):
+        self.token_pattern_str = '|'.join([f"(?P<{key}>{value})" for key, value in patterns.items()])
+        self.re_pattern = re.compile(self.token_pattern_str)
+        if not self.re_pattern:
+            raise ValueError("Unable to compile provided patterns.")
+        
+    def load_files(self, files: List[str]):
+        self.files = files
+        for file in self.files:
+            with open(file, 'r') as source:
+                lines = source.readlines()
+                self.lines.append(lines)
+
+    def config(self, **flags):
+        for key, value in flags:
+            self.config[key] = value
+
+    def get_position(self):
+        return self.files[self.file_index], self.line, self.column + 1
+
+    def peek(self):
+        while True:
+            if (self.skip_white_space and self.expect_type('WHITE_SPACE') or \
+            self.skip_EOF and self.expect_type('EOF')) and \
+            not self.is_end():
+                self.consume()
+                continue
+            break
+        return None if self.is_end() else self.lines[self.line][self.column]
+
+    def expect_value(self, expected_sting: str, lower: bool = False):
+        return self.peek().value.tolower() == expected_sting if lower else self.peek().value == expected_sting
+
+    def expect_type(self, expected_type: str):
+        return self.peek().type == expected_type
+
+    def consume(self):
+        if self.expect_type('EOF'):
+            self.file_index += 1
+        self.column += 1
+        if self.column > len(self.lines[self.line]):
+            self.consume_line()
+
+    def backtrack(self):
+        pass
+
+    def is_end(self):
+        return self.line > len(self.lines)
+
+    def consume_line(self):
+        if self.is_end():
+            raise IndexError("Attempt to consume line beyond end of program.")
+        self.line += 1
+        self.column = 0
---
+++ readme.md
@@ -0,0 +1,47 @@
+# Tokenizer
+
+This is a simple tokenizer in Python 3. It is in two layers:
+- A line tokenizer
+- A token buffer that handles one or more files
+
+The tokenizer automatically provides 'wHITE_SPACE' and 'UNKNOWN'. White space is collected together and unknown tokens are individual characters.
+
+Token_Buffer.config offers access to tokenizing flags:
+- skip_white_space: peek will not return whitespace.
+- include_EOF: The end of file is marked for the sake of tracking position. include_EOF causes peek to NOT skip EOF. This is handy for files that have ignored preamble.
+
+Example program:
+```python
+import TokenBuffer as tb
+import sys
+
+if len(sys.argv) < 2:
+    print('Please provide one or more files to tokenize')
+    exit(1)
+
+patterns = {
+    "INTEGER": r"\d+",
+    "ASSIGNMENT": r"=",
+    "PLUS": r"\+",
+    "MINUS": r"\-",
+    "MULTIPLY": r"\*",
+    "DIVIDE": r"\\",
+    "EXPONENT": r"\^",
+    "TETRATION": r"\^\^",
+    "IDENTIFIER": r"[\w_],
+    "OPEN_PAREN": r"\(",
+    "CLOSE_PAREN": r"\)",
+}
+
+buffer = tb.TokenBuffer()
+buffer.init_patterns(patterns)
+buffer.load_files(sys.argv[1:])
+buffer.config(skip_white_space = True)
+buffer.tokenize()
+
+while not buffer.out_of_tokens():
+    peek = buffer.peek()
+    file, line, column = buffer.get_position()
+    print(f'{file} {line} {column} Token: type = {peek.type}, value = {peek.value}')
+    buffer.consume()
+```
